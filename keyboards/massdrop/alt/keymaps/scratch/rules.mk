efc12d391bfffb9705ccf811306c1ec4731c7a9a
Mark Becwar <mark@thebecwar.com>
2019-10-13 20:32:22 -0400

    Drop Alt Scratch

---
+++ keyboards/massdrop/alt/keymaps/scratch/rules.mk
@@ -0,0 +1,25 @@
+# RGB_MATRIX_CUSTOM_USER
+RGB_MATRIX_CUSTOM_USER = no
+
+# Animation framebuffer effects
+OPT_DEFS += -DRGB_MATRIX_FRAMEBUFFER_EFFECTS
+#Enable all additional animation modes.
+OPT_DEVS += -DRGBLIGHT_ANIMATIONS 
+#Enable alternating animation mode.
+OPT_DEVS += -DRGBLIGHT_EFFECT_ALTERNATING 
+#Enable breathing animation mode.
+OPT_DEVS += -DRGBLIGHT_EFFECT_BREATHING 
+#Enable christmas animation mode.
+OPT_DEVS += -DRGBLIGHT_EFFECT_CHRISTMAS 
+#Enable knight animation mode.
+OPT_DEVS += -DRGBLIGHT_EFFECT_KNIGHT 
+#Enable rainbow mood animation mode.
+OPT_DEVS += -DRGBLIGHT_EFFECT_RAINBOW_MOOD 
+#Enable rainbow swirl animation mode.
+OPT_DEVS += -DRGBLIGHT_EFFECT_RAINBOW_SWIRL 
+#Enable RGB test animation mode.
+OPT_DEVS += -DRGBLIGHT_EFFECT_RGB_TEST 
+#Enable snake animation mode.
+OPT_DEVS += -DRGBLIGHT_EFFECT_SNAKE 
+#Enable static gradient mode.
+OPT_DEVS += -DRGBLIGHT_EFFECT_STATIC_GRADIENT 
